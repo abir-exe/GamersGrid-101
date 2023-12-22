40d4f41b5220fb237b25cfc19a44b96ef772bc3f
Abir <abirmahmud221@gmail.com>
2023-12-22 16:06:26 +0600

    deployment issues solved

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => {
-    res.send('boss is sitting')
+    res.send('GamersGrid is Running')
 })
-
+ 
 app.listen(port, () => {
     console.log(`GamersGrid is running on port ${port}`)
 })
