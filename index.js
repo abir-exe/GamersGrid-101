9e8dcf7c1b2e980e7cf4c500356fc1253da4f554
Abir <abirmahmud221@gmail.com>
2023-12-25 10:29:06 +0600

    jwt and apis implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -27,6 +28,15 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const taskCollection = client.db("gamersGrid").collection("tasks");
+
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "1hr",
+        });
+        res.send({ token });
+      });
 
     // task related api 
     app.get("/tasks", async(req, res) => {
