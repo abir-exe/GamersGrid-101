efc6d76a1f51080d81e8926698fab9af1f1dc2e6
Abir <abirmahmud221@gmail.com>
2023-12-25 11:47:12 +0600

    task deleting options added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -43,6 +43,20 @@ async function run() {
         const result = await taskCollection.find().toArray();
         res.send(result);
       })
+    
+    app.post("/tasks", async (req, res) => {
+        const item = req.body;
+        const result = await taskCollection.insertOne(item);
+        res.send(result);
+      });
+
+      app.delete('/tasks/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await taskCollection.deleteOne(query);
+        res.send(result); 
+    })
+    
     
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
